c9ae36ff22bc20be887d360d0089077717540a03
Eziokwubundu Chibueze <Nathan-E@Eziokwubundus-MacBook-Pro.local>
2019-02-15 11:22:57 +0100

    test cases for the producer constructor failing

--- participant/producer/producer.test.js
+++ participant/producer/producer.test.js
@@ -1,1 +1,12 @@
-const producer = require('./producer');
+const Producer = require('./producer');
+
+const Pharmacy = new Producer('Producer');
+const Paracetamol = new Producer('Producer')
+
+test('Checks if the Producer class can instantiate', () => {
+  expect(Pharmacy).toBeInstanceOf(Producer);
+});
+
+test('Ensures that only one instance of the Producer class exist', () => {
+  expect(Pharmacy === Paracetamol).toBeTruthy();
+});
