eef9b356d86d9f06f665000a4ca109114f228212
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 20:10:50 +0100

    Test cases for the StateFactory Class failing

--- state_factory/state_factory.test.js
+++ state_factory/state_factory.test.js
@@ -18,5 +18,18 @@ const StateFactory = require('./state_factory');
 const stateFactory = new StateFactory(this);
 
 test('Ensures the StateFactory Class returns an instance of the specified type', () => {
-  expect()
+  const state = stateFactory.initailState('Producer', 'Panadol');
+  expect(state).toBeInstanceOf(Producer);
+});
+test('Ensures the StateFactory Class returns an instance of the specified type', () => {
+  const state = stateFactory.initailState('Retailer', 'Panadol');
+  expect(state).toBeInstanceOf(Retailer);
+});
+test('Ensures the StateFactory Class returns an instance of the specified type', () => {
+  const state = stateFactory.initailState('Consumer', 'Panadol');
+  expect(state).toBeInstanceOf(Consumer);
+});
+test('Ensures the StateFactory Class returns an instance of the specified type', () => {
+  const state = stateFactory.initailState('Recycler', 'Panadol');
+  expect(state).toBeInstanceOf(Recycler);
 });
