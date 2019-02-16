8db37fecdab84dcce9e1b085e9ba5d2f38262529
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:51:50 +0100

    Testcases for Recycler Class passed

--- participants/recycler/recycler.test.js
+++ participants/recycler/recycler.test.js
@@ -7,3 +7,6 @@ test('Checks if the Retailer class can instantiate', () => {
   expect(Moses).toBeInstanceOf(Recycler);
 });
 
+test('Ensures that several instances of the Retailer class can exist', () => {
+  expect(Grace === Moses).toBeFalsy();
+});
