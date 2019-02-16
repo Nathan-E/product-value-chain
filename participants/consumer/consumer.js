39b85dabe96f3efa378723e8c77edc3c43f7ada2
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:41:07 +0100

    Creating the Consumer Class

---
+++ participants/consumer/consumer.js
@@ -0,0 +1,15 @@
+class Consumer {
+  constructor(state, product){
+    this.product = product;
+    this.name = 'Consumer';
+    this.nextState = 'Recycler';
+    this.flowChannel = (productName) => {
+      const data = `Channelling ${this.product} from the ${this.name} to the ${this.nextState}`;
+      //chains the next state
+      fs.appendFileSync('../data/result.txt', `${data}\n`);
+      console.log(data);
+      state.changeState(new Recycler(state, product));
+    }
+  }
+}
+
