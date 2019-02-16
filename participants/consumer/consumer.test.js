1bc334f7d681f710e8b452dc5a7a95de01eaac0f
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:39:20 +0100

    Testcases for Consumer Class failing

--- participants/consumer/consumer.test.js
+++ participants/consumer/consumer.test.js
@@ -7,3 +7,6 @@ test('Checks if the Retailer class can instantiate', () => {
   expect(Dare).toBeInstanceOf(Consumer);
 });
 
+test('Ensures that several instances of the Retailer class can exist', () => {
+  expect(Kazeem === Dare).toBeFalsy();
+});
