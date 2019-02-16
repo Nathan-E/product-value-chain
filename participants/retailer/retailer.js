57a8ac9b208f42727b4c0747bde4180a3c819663
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:45:52 +0100

    Testcases for Retailer Class passed

---
+++ participants/retailer/retailer.js
@@ -0,0 +1,14 @@
+class Retailer {
+  constructor(state, product) {
+    this.product = product;
+    this.name = 'Retailer';
+    this.nextState = 'Consumer';
+    this.flowChannel = (productName) => {
+      const data = `Channelling ${this.product} from the ${this.name} to the ${this.nextState}`;
+      //chains the next state
+      fs.appendFileSync('../data/result.txt', `${data}\n`);
+      console.log(data);
+      state.changeState(new Consumer(state, product));
+    }
+  }
+}
