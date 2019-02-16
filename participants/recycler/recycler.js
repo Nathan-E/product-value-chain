14de5e224509d246ffb3ed7a25a264cb26d65fe1
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:53:07 +0100

    Creating the Recycler Class

---
+++ participants/recycler/recycler.js
@@ -0,0 +1,15 @@
+class Recycler {
+  constructor(state, product) {
+    this.product = product;
+    this.name = 'Recycler';
+    this.nextState = 'Producer';
+    this.flowChannel = (productName) => {
+      const data = `Channelling recycled ${this.product} from the ${this.name} to the ${this.nextState}`;
+      //chains the next state
+      fs.appendFileSync('../data/result.txt', `${data}\n`);
+      console.log(data);
+      // state.changeState(new Producer(state, this.product));
+
+    }
+  };
+}
