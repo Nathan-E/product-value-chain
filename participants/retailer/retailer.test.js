ed75d70f4f9403332fe89527e00a52abb23bb222
Nathan-E <eziokwubunduchibueze.com>
2019-02-16 19:43:40 +0100

    Testcases for Retailer Class failed

--- participants/retailer/retailer.test.js
+++ participants/retailer/retailer.test.js
@@ -7,3 +7,6 @@ test('Checks if the Retailer class can instantiate', () => {
   expect(paxPharmacy).toBeInstanceOf(Retailer);
 });
 
+test('Ensures that several instances of the Retailer class can exist', () => {
+  expect(paxPharmacy === wellPharmacy).toBeFalsy();
+});
