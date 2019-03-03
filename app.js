b4e342b630feeac55de24e16c214804f3047d807
Nathan-E <eziokwubunduchibueze.com>
2019-03-03 18:01:50 +0100

    entry point of the app

---
+++ app.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const run = require('./processor/runner/runner');
+let check = require('./monitor/monitor');
+
+const regex = /[\,\/\)\&\*\=\>\{\}\'\"\[\]\ˆ\%\$\#\@\!\`\˜\.\<\;\:\_\+\=\(\-\\\/\|]/g;
+
+const id = (function* IdGen() {
+  let x = 0;
+  while (true) yield ++x;
+})();
+
+const id2 = (function* IdGen() {
+  let x = 0;
+  while (true) yield ++x;
+})();
+
+let idw = null;
+
+//reads the input file
+function read() {
+  data = fs.readFileSync('./data/input.txt', 'utf-8').replace(regex, '').split('\n')
+  let result = data.length;
+
+  data.forEach(function (line) {
+    idw = id2.next().value;
+
+    product = line.replace(/[^\w\s]/gm, '')
+      .replace(/\s\s+/gm, ' ')
+      .trim()
+      .split(' ');
+    if (product.length > 1) {
+      //concatnate the product name and it state...into the run function
+      let id1 = id.next().value
+      run([id1, ...product.slice(0, 2)].join(' '), product[2], './data/output.txt');
+    }
+  });
+  if (result === idw) {
+    check.value = true;
+  };
+
+};
+
+
+read();
