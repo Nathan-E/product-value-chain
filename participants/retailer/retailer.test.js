83a6bc95415192c1d96f468fba0f8ca29718f736
Nathan-E <eziokwubunduchibueze.com>
2019-03-03 17:57:49 +0100

    testing the fs module appendFile method

--- participants/retailer/retailer.test.js
+++ participants/retailer/retailer.test.js
@@ -1,12 +1,15 @@
+jest.mock('fs');
 const Retailer = require('./retailer');
+const fs = require('fs');
 
 const paxPharmacy = new Retailer('Retailer');
-const wellPharmacy = new Retailer('Retailer')
 
 test('Checks if the Retailer class can instantiate', () => {
   expect(paxPharmacy).toBeInstanceOf(Retailer);
 });
 
-test('Ensures that several instances of the Retailer class can exist', () => {
-  expect(paxPharmacy === wellPharmacy).toBeFalsy();
-});
+test('Mocks the fs module', () => {
+const wellPharmacy = new Retailer(this, 'syrup')
+  wellPharmacy.flowChannel('syrup', 'url');
+  expect(fs.database['url'].trim() === 'syrup from Retailer to Consumer').toBeTruthy();
+})
