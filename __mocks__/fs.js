bee72393ffa792c268b3ebcd7bc75c945f0bffe6
Nathan-E <eziokwubunduchibueze.com>
2019-03-04 05:35:50 +0100

    adding more comments to code base

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,15 +1,9 @@
+//mocks the output text 
 const database = {}
 
+//mocks the appendFile method of the fs module
 function appendFile(link, data){
   database[link] = data;
 }
 
-// appendFile(database, 'Hello');
-// console.log(database.includes('Hello'));
-
-// function readFile(link, option){
-//   return database[link];
-// }
-
 module.exports = {database, appendFile};
-// module.exports = fs;
--- monitor/monitor.js
+++ monitor/monitor.js
@@ -1,1 +1,2 @@
+//global variable used to stop the program in real time
 module.exports = {value: null};
