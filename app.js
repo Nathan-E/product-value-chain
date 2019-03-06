05ad7bd54c3fa83a57ffd937b1ae1d06debc3a62
Nathan-E <eziokwubunduchibueze.com>
2019-03-06 13:18:44 +0100

    refactoring app.js WIP

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const run = require('./processor/runner/runner');
 let check = require('./monitor/monitor');
 
-const id = (function* IdGen() {
+const productId= (function* IdGen() {
   let x = 0;
   while (true) yield ++x;
 })();
@@ -17,7 +17,6 @@ let idw = null;
 //reads the input file
 function read() {
   let data = fs.readFileSync('./data/input.txt', 'utf-8').replace(/[^\w\s]/gm, '').split('\n');
-  // data = fs.readFileSync('./data/input.txt', 'utf-8').replace(regex, '').split('\n')
   let result = data.length;
 
   data.forEach(function (line) {
@@ -29,8 +28,7 @@ function read() {
       .split(' ');
     if (product.length > 1) {
       //concatnate the product name and it state...into the run function
-      let id1 = id.next().value;
-      run([id1, ...product.slice(0, 2)].join(' '), product[2], './data/output.txt');
+      run([productId.next().value, ...product.slice(0, 2)].join(' '), product[2], './data/output.txt');
     }
   });
   if (result === idw) {
